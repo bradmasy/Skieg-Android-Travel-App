97f650163474b45a1dd4515a1d0980a7fd5bd22e
Brad <bmasciotra@my.bcit.ca>
2022-10-03 11:18:42 -0700

    base activity and test playground for maps api

---
+++ app/src/main/java/skieg/travel/Maps.java
@@ -0,0 +1,15 @@
+package skieg.travel;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class Maps extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maps_activity);
+    }
+}
