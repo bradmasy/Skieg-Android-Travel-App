7baebf2f5fb82d212294b36ae43326c424eb76cf
Alexander Farlinger <alexanderfarlinger@gmail.com>
2022-10-04 14:49:56 -0700

    added connections between all activities through the maina nd planner activities. Main connects to:  - Events  - Planner  - Calendar  - Personal Profile Planner connects to:  - Budget  - Checklist  - Itinerary  - Journal
     user profile has no connections yet

--- app/src/main/java/skieg/travel/MainActivity.java
+++ app/src/main/java/skieg/travel/MainActivity.java
@@ -2,7 +2,11 @@ package skieg.travel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import skieg.travel.planner.PlannerActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickMainEventsButton(View view) {
+       Intent intent = new Intent(this, EventsActivity.class);
+       startActivity(intent);
+    }
+
+    public void clickMainPlannerButton(View view) {
+        Intent intent = new Intent(this, PlannerActivity.class);
+        startActivity(intent);
+    }
+
+    public void clickMainCalendarButton(View view) {
+        Intent intent = new Intent(this, CalendarActivity.class);
+        startActivity(intent);
+    }
+
+    public void clickMainPersonalProfileButton(View view) {
+        Intent intent = new Intent(this, PersonalProfileActivity.class);
+        startActivity(intent);
+    }
+
+    public void clickMainPostButton(View view) {
+        Intent intent = new Intent(this, PostActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/skieg/travel/planner/PlannerActivity.java
+++ app/src/main/java/skieg/travel/planner/PlannerActivity.java
@@ -1,9 +1,12 @@
 package skieg.travel.planner;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import skieg.travel.EventsActivity;
 import skieg.travel.R;
 
 public class PlannerActivity extends AppCompatActivity {
@@ -12,5 +15,25 @@ public class PlannerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_planner);
+    }
+
+    public void clickPlannerBudgetButton(View view) {
+        Intent intent = new Intent(this, EventsActivity.class);
+        startActivity(intent);
+    }
+
+    public void clickPlannerChecklistButton(View view) {
+        Intent intent = new Intent(this, EventsActivity.class);
+        startActivity(intent);
+    }
+
+    public void clickPlannerItineraryButton(View view) {
+        Intent intent = new Intent(this, EventsActivity.class);
+        startActivity(intent);
+    }
+
+    public void clickPlannerJournalButton(View view) {
+        Intent intent = new Intent(this, EventsActivity.class);
+        startActivity(intent);
     }
 }
