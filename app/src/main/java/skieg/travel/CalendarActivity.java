1b7dd4f55d678baf745651201a5bee0275fdb8da
Monica Bacatan <monicab3002@outlook.com>
2022-10-10 09:45:04 -0700

    Added different layouts with basic format for events and calendar

--- app/src/main/java/skieg/travel/CalendarActivity.java
+++ app/src/main/java/skieg/travel/CalendarActivity.java
@@ -1,6 +1,8 @@
 package skieg.travel;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,10 @@ public class CalendarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+    }
+
+    public void clickMainButton(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
