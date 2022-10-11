322293146fb437ed56d7256fd8d31e122e4e063c
Brad <bmasciotra@my.bcit.ca>
2022-10-11 15:15:25 -0700

    working on layout for post/forum

--- app/src/main/java/skieg/travel/PostActivity.java
+++ app/src/main/java/skieg/travel/PostActivity.java
@@ -1,6 +1,10 @@
 package skieg.travel;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,23 @@ public class PostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+        Button postButton = findViewById(R.id.makePost);
+        LinearLayout userInfo = findViewById(R.id.userInfo);
+        LinearLayout userImageContainer = findViewById(R.id.userPhoto);
+        Button filler = findViewById(R.id.filler);
+        // setting colors
+        postButton.setBackgroundColor(Color.parseColor("#BEDEFC"));
+        userInfo.setBackgroundColor(Color.parseColor("#BEDEFC"));
+        userImageContainer.setBackgroundColor(Color.parseColor("#BEDEFC"));
+        filler.setBackgroundColor(Color.parseColor("#BEDEFC"));
+
+
+
+
+
+
     }
+
+
+
 }
