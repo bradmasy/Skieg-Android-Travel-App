f92caf9a642bf6703404ada6339c41b304d1f64c
Brad <bmasciotra@my.bcit.ca>
2022-11-06 09:03:11 -0800

    updated login and signup

--- app/src/main/java/skieg/travel/welcome_login_signup/Login.java
+++ app/src/main/java/skieg/travel/welcome_login_signup/Login.java
@@ -5,13 +5,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import skieg.travel.MainActivity;
 import skieg.travel.R;
@@ -19,6 +25,10 @@ import skieg.travel.R;
 public class Login extends AppCompatActivity {
 
     private FirebaseAuth authentication;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+    private DataSnapshot snapshot;
+
 
 
     @Override
@@ -26,6 +36,8 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         authentication = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference();
 
 
 
@@ -38,7 +50,42 @@ public class Login extends AppCompatActivity {
 
     private void logUserIn(View view) {
 
+        EditText username = findViewById(R.id.username);
+        EditText password = findViewById(R.id.userPassword);
 
+        String usernameString = username.getText().toString();
+        String passwordString = password.getText().toString();
+
+
+
+        if (usernameString.isEmpty() || passwordString.isEmpty()) {
+            Toast.makeText(this, "Please make sure to fill in your email and password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        snapshot = databaseReference.get().getResult();
+        Log.d("Snap: ", snapshot.toString());
+//        databaseReference.child("Users").get().onSuccessTask(task->{
+
+//            if(task.exists()){
+//                Log.d("Task: ", task.toString());
+//            }
+//            return null;
+//        });
+
+//        Log.d("Users: ", databaseReference.child("Users").toString());
+
+
+//    {
+//                task ->
+//                if (task.isSuccessful) {
+//                    updateFirebaseUserDisplayName()
+//                } else {
+//                    Toast.makeText(this, "An error has occurred during login. Please try again later.", Toast.LENGTH_SHORT).show()
+//                }
+//            }
+//
+//    }
     }
 
 
--- app/src/main/java/skieg/travel/welcome_login_signup/Signup.java
+++ app/src/main/java/skieg/travel/welcome_login_signup/Signup.java
@@ -2,16 +2,29 @@ package skieg.travel.welcome_login_signup;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import skieg.travel.MainActivity;
 import skieg.travel.R;
 
 public class Signup extends AppCompatActivity {
+
+    private FirebaseAuth authentication;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,6 +33,8 @@ public class Signup extends AppCompatActivity {
 
         Button backButton = findViewById(R.id.backButton);
         backButton.setOnClickListener(this::backBtnClicked);
+        authentication = FirebaseAuth.getInstance();
+
     }
 
 
@@ -27,4 +42,25 @@ public class Signup extends AppCompatActivity {
         Intent mainIntent = new Intent(this, Welcome.class);
         startActivity(mainIntent);
     }
+
+
+//    public void signUp(View view){
+//
+//
+//        EditText firstNameInput = findViewById(R.id.firstName);
+//        EditText lastNameInput = findViewById(R.id.lastName);
+//        EditText usernameInput = findViewById(R.id.username);
+//        EditText emailInput = findViewById(R.id.userEmail);
+//        EditText passwordInput = findViewById(R.id.userPassword);
+//
+//        String firstName = firstNameInput.getText().toString();
+//        String lastName = lastNameInput.getText().toString();
+//        String username = usernameInput.getText().toString();
+//        String email = emailInput.getText().toString();
+//        String password = passwordInput.getText().toString();
+//
+//
+//
+//}
 }
+
