48f71fe219cce6874ceb04e7bfd9f588ff7e31c1
Brad <bmasciotra@my.bcit.ca>
2022-11-12 21:15:27 -0800

    post is grabbing and rendering fine on page

--- app/src/main/java/skieg/travel/PostActivity.java
+++ app/src/main/java/skieg/travel/PostActivity.java
@@ -3,6 +3,7 @@ package skieg.travel;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -20,19 +22,29 @@ import androidx.fragment.app.ListFragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 import skieg.travel.forum.ForumBoardFragment;
+import skieg.travel.post.Post;
 import skieg.travel.post.PostAdapter;
+import skieg.travel.post.PostFragment;
 import skieg.travel.post.PostPage;
 
 
 public class PostActivity extends AppCompatActivity {
 
-
-
-    final public static int MARGIN_BOTTOM = 5;
-    final public static int MARGIN_TOP = 5;
-    final public static int MARGIN_LEFT = 5;
-    final public static int MARGIN_RIGHT = 5;
+    PostAdapter PDAdapter;
+    PostFragment postFragment;
+    DatabaseReference db;
+    ArrayList<String> names= new ArrayList<>();
+    ArrayList<String> posts= new ArrayList<>();
+    ArrayList<String> dates = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,44 +61,70 @@ public class PostActivity extends AppCompatActivity {
         userImageContainer.setBackgroundColor(Color.parseColor("#BEDEFC"));
         filler.setBackgroundColor(Color.parseColor("#BEDEFC"));
 
+        postFragment = new PostFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.recyclerPosts ,postFragment);
+        fragmentTransaction.commit();
+        getDataFromFirebase();
 
+    }
 
-
-        Fragment forum = new ForumFragment();
-
-        //FragmentManager fragmentManager = getSupportFragmentManager();
-
-
-//        LinearLayout postScroll = findViewById(R.id.postScroll);
-//        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-//                LinearLayout.LayoutParams.WRAP_CONTENT);
-//        posts = getResources().getStringArray(R.array.postTest); // should be dynamic from database wen connected
+    private void getDataFromFirebase() {
+        // all the posts in line 1
+        db = FirebaseDatabase.getInstance("https://skieg-364814-default-rtdb.firebaseio.com/").getReference().child("Forum").child("posts");
 //
-//        for(int i = 0; i < posts.length; i++){
-//            TextView post = new TextView(this);
-//            post.setBackgroundColor(Color.parseColor("#BEDEFC"));
-//            post.setText(posts[i]);
-//
-//            post.setLayoutParams(params);
-//
-//            params.setMargins(MARGIN_LEFT, MARGIN_TOP, MARGIN_RIGHT, MARGIN_BOTTOM);
-//            postScroll.addView(post);
-//
-//        }
+//        names.add("Brad");
+//        posts.add("this is my new post");
+//        dates.add("11/12/2022");
 
+        db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.d("LOG", "IN DATA SNAPSHOT METHOD");
+
+
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+
+                    Log.d("Data Snap: ", dataSnapshot.getValue().toString());
+
+
+                    //
+//
+                    String currSnapshot = String.valueOf(dataSnapshot.getValue());
+
+                    String[] dataValues = currSnapshot.split(",");
+//
+                    String date = DatabaseParse.parseDataValue(dataValues[0]);
+                    Log.d("DATE:", date);
+                    String name = DatabaseParse.parseDataValue(dataValues[1]);
+                    Log.d("DESCRIBE:", name);
+                    String content = DatabaseParse.parseLastDataValue(dataValues[2]);
+                    Log.d("Content:", content);
+
+                    names.add(name);
+                    dates.add(date);
+                    posts.add(content);
+                }
+
+
+                // render here...
+
+                PDAdapter = new PostAdapter(names, posts, dates);
+                //PostFragment postFragment = new PostFragment();
+                postFragment.initializeAdapter(PDAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("LOG", "DB ERROR");
+            }
+        });
     }
 
 
     public void makePost(View view){
         Intent intent = new Intent(PostActivity.this, PostPage.class);
         startActivity(intent);
-
     }
-
-
-
-
-
-
-
 }
--- app/src/main/java/skieg/travel/post/PostFragment.java
+++ app/src/main/java/skieg/travel/post/PostFragment.java
@@ -11,18 +11,24 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+
 import skieg.travel.R;
 
 public class PostFragment extends Fragment {
 
 
     private RecyclerView recyclerView;
+    DatabaseReference db;
     PostAdapter postAdapter;
 
+    // GRAB FROM DATA BASE FOR THIS.
     String[] posts;
-    String[] names;
-    String[] content;
-    String[] dates;
+    ArrayList<String> names = new ArrayList<>();
+    ArrayList<String> content = new ArrayList<>();
+    ArrayList<String> dates = new ArrayList<>();
 
     public PostFragment(){
 
@@ -32,11 +38,18 @@ public class PostFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.recyclerview_layout,container,false);
-        recyclerView = view.findViewById(R.id.recyclerPosts);
+        recyclerView = view.findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager((view.getContext())));
+
         postAdapter = new PostAdapter(names,content,dates);
         recyclerView.setAdapter(postAdapter);
 
         return view;
     }
+
+    public void initializeAdapter(RecyclerView.Adapter<PostAdapter.ViewHolder> adapter){
+        recyclerView.setAdapter(adapter);
+    }
+
+
 }
