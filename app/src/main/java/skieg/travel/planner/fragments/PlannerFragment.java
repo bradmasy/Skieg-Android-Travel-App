cdeedbc623baac9a260447deeb6515d981c889c6
Brad <bmasciotra@my.bcit.ca>
2022-11-27 19:23:49 -0800

    recyler view budget

--- app/src/main/java/skieg/travel/planner/fragments/PlannerFragment.java
+++ app/src/main/java/skieg/travel/planner/fragments/PlannerFragment.java
@@ -7,12 +7,25 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+/**
+ * Planner fragment class.
+ */
 public class PlannerFragment extends Fragment {
 
-
+    /**
+     * Planner fragment constructor.
+     *
+     * @param implementation the implementation id.
+     */
     public PlannerFragment(int implementation){
         super(implementation);
     }
+
+    /**
+     * On view created.
+     * @param view a view
+     * @param savedInstanceState a bundle representing the previously saved instance data.
+     */
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
--- app/src/main/java/skieg/travel/planner/fragments/RecyclerViewBudget.java
+++ app/src/main/java/skieg/travel/planner/fragments/RecyclerViewBudget.java
@@ -12,12 +12,22 @@ import java.util.ArrayList;
 import skieg.travel.R;
 import skieg.travel.post.RecyclerViewClickListener;
 
+/**
+ * Recycler View Budget Class.
+ */
 public class RecyclerViewBudget extends RecyclerView.Adapter<RecyclerViewBudget.MyViewHolder> {
 
     public ArrayList<String> items;
     public ArrayList<Double> amounts;
     public ArrayList<String> IDs;
 
+    /**
+     * RecyclerViewBudget Constructor.
+     *
+     * @param items list of items.
+     * @param amounts list of amounts.
+     * @param IDs list of ids.
+     */
     public RecyclerViewBudget(ArrayList<String> items, ArrayList<Double> amounts, ArrayList<String> IDs) {
         this.items = items;
         this.amounts = amounts;
@@ -25,6 +35,13 @@ public class RecyclerViewBudget extends RecyclerView.Adapter<RecyclerViewBudget.
     }
 
 
+    /**
+     * On Create View Holder.
+     *
+     * @param parent the parent view group.
+     * @param viewType the type of view.
+     * @return a view holder.
+     */
     @NonNull
     @Override
     public RecyclerViewBudget.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -32,14 +49,23 @@ public class RecyclerViewBudget extends RecyclerView.Adapter<RecyclerViewBudget.
         return new RecyclerViewBudget.MyViewHolder(view);
     }
 
-
+    /**
+     * On Bind View Holder.
+     *
+     * @param holder a view holder.
+     * @param position the position.
+     */
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         holder.item.setText(items.get(position));
         holder.amount.setText(String.valueOf(amounts.get(position)));
     }
 
-
+    /**
+     * Gets the item count.
+     *
+     * @return the item count.
+     */
     @Override
     public int getItemCount() {
         return items.size();
@@ -51,11 +77,15 @@ public class RecyclerViewBudget extends RecyclerView.Adapter<RecyclerViewBudget.
 
         TextView item, amount;
 
+        /**
+         * My View Holder constructor.
+         *
+         * @param itemView the item view.
+         */
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             item = itemView.findViewById(R.id.item);
             amount = itemView.findViewById(R.id.amount);
         }
-
     }
 }
