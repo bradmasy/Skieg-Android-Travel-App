f0d699f95ce42e44b0d7f0d1d532ff980afe9751
Monica Bacatan <monicab3002@outlook.com>
2022-11-27 19:47:38 -0800

    Commented create calendar event and fragment

--- app/src/main/java/skieg/travel/CalendarFragment.java
+++ app/src/main/java/skieg/travel/CalendarFragment.java
@@ -13,23 +13,39 @@ import java.util.ArrayList;
 
 public class CalendarFragment extends Fragment {
 
+    // Stores recyclerview references
     private RecyclerView recyclerView;
     RecyclerViewCalendar recyclerViewCalendar;
 
+    // Stores values for each calendar event attribute
     ArrayList<String> titles = new ArrayList<>();
     ArrayList<String> descriptions = new ArrayList<>();
     ArrayList<String> locations = new ArrayList<>();
     ArrayList<String> dates = new ArrayList<>();
 
+    /**
+     * Required empty public constructor
+     */
     public CalendarFragment() {
-        // Required empty public constructor
     }
 
+    /**
+     * When calendar fragment is created.
+     * @param savedInstanceState: bundle
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    /**
+     * Creates a view object using the recyclerview XML layout.
+     * Sets the recycler view adapter to the custom recyclerview calendar.
+     * @param inflater: LayoutInflater
+     * @param container: ViewGroup
+     * @param savedInstanceState: bundle
+     * @return view object
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.recyclerview_layout, container, false);
@@ -42,7 +58,10 @@ public class CalendarFragment extends Fragment {
         return view;
     }
 
-    // Sets a RecyclerViewProduct adapter to this fragment's recycler view
+    /**
+     * Sets a RecyclerViewProduct adapter to this fragment's recycler view
+     * @param adapter: RecyclerView Adapter
+     */
     public void initializeAdapter(RecyclerView.Adapter<RecyclerViewCalendar.MyViewHolder> adapter) {
         recyclerView.setAdapter(adapter);
     }
--- app/src/main/java/skieg/travel/CreateCalendarEventActivity.java
+++ app/src/main/java/skieg/travel/CreateCalendarEventActivity.java
@@ -18,28 +18,38 @@ import skieg.travel.Utility.InputValidation;
 
 public class CreateCalendarEventActivity extends AppCompatActivity {
 
+    // Store references to the firebase database
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
 
+    // Button for creating a calendar event
     Button createEventBtn;
 
+    // EditText objects for getting
     EditText titleInput;
     EditText descriptionInput;
     EditText locationInput;
     EditText dateInput;
 
+    /**
+     * When create calendar event activity page is created.
+     * @param savedInstanceState: bundle
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_calendar_event);
 
+        // Add a click listener for creating a calendar event
         createEventBtn = findViewById(R.id.createEventBtn);
         createEventBtn.setOnClickListener(view -> {
+            // Get the objects on the XML layout
             titleInput = findViewById(R.id.title);
             descriptionInput = findViewById(R.id.description);
             locationInput = findViewById(R.id.location);
             dateInput = findViewById(R.id.date);
 
+            // Get the values from the EditText fields
             String title = titleInput.getText().toString();
             String description = descriptionInput.getText().toString();
             String location = locationInput.getText().toString();
@@ -51,18 +61,27 @@ public class CreateCalendarEventActivity extends AppCompatActivity {
                 Toast toast = Toast.makeText(getApplicationContext(), "Please fill in all fields", Toast.LENGTH_LONG);
                 toast.show();
 
+                // MAke sure date field is valid
             } else if (InputValidation.invalidDateInput(date)) {
                 Toast toast = Toast.makeText(getApplicationContext(), "Date must have the format: YYYY-MM-DD and be valid", Toast.LENGTH_LONG);
                 toast.show();
 
             } else {
-                writeUserToFirebase(title, description, location, date);
+                // All data is valid so the new calendar event can be added to the firebase
+                writeEventToFirebase(title, description, location, date);
             }
         });
     }
 
 
-    public void writeUserToFirebase(String title, String description, String location, String date) {
+    /**
+     * Creates a calendar event and writes it to the database.
+     * @param title: String
+     * @param description: String
+     * @param location: String
+     * @param date: String
+     */
+    public void writeEventToFirebase(String title, String description, String location, String date) {
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = FirebaseDatabase.getInstance("https://skieg-364814-default-rtdb.firebaseio.com/").getReference();
 
@@ -86,6 +105,10 @@ public class CreateCalendarEventActivity extends AppCompatActivity {
     }
 
 
+    /**
+     * When back button is clicked, redirect to calendar activity.
+     * @param view: View
+     */
     public void backBtnClicked(View view) {
         Intent mainIntent = new Intent(this, CalendarActivity.class);
         startActivity(mainIntent);
